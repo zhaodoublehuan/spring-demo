98da6361b412417b7147cb6777ae5f89a5c00fe4
zhaohuanhuan <zhaohuanhuan@shsnc.com>
2019-09-25 17:15:02 +0800

    修改引用包

---
+++ src/main/java/com/tower/SpringDemoApplication.java
@@ -0,0 +1,16 @@
+package com.tower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tower
+ */
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+}
